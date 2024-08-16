47ba9623120d3b2dad378a2e1046cbac18ed5c6a
João Pedro da Silva <34964281+Pedro2080@users.noreply.github.com>
2024-08-16 16:46:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-
+console.log(import.meta.env.REACT_APP_VITE_MESSAGE)
 function App() {
-  console.log(import.meta.env.REACT_APP_VITE_MESSAGE)
+  
   return (
     <main className="max-w-[932px] pb-8 pt-16 px-10">
       <h1 className="mb-6 md:text-6xl reckless text-4xl text-white">
